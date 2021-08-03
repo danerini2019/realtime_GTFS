2915c08aea8016a733e9d9ec5f1044c3f4bcf559
Dane Rini <danerini@gmail.com>
2021-08-02 22:35:19 -0700

    pprint addition

--- peartree_demo.py
+++ peartree_demo.py
@@ -8,17 +8,19 @@ import osmnx as ox
 import numpy as np
 import peartree as pt
 from shapely.geometry import Point
+import pprint
 
 # Bay area rectangle select
-tl_query = 'https://transit.land/api/v1/feeds?bbox=38.055199,-122.926353,37.252472,-121.672981'
+tl_query = 'https://transit.land/api/v1/operators?bbox=-122.503607,37.166611,-121.713958,38.038060'
 
 # Brooklyn example rectangle select
-# tl_query = 'https://transit.land/api/v1/feeds?bbox=-73.97339,40.649778,-73.946532,40.670353' 
+# tl_query = 'https://transit.land/api/v1/stops?bbox=-73.97339,40.649778,-73.946532,40.670353' 
+
 header = {'apikey':'NMmVInTVH1ctN7deZOcDy2Wzbh53GVav'}
 
 resp = requests.get(tl_query, headers=header)
 rj = resp.json()
-print(rj)
+pprint.pprint(rj['stops'][10])
 
 # zip_url = None
 # for f in rj['feeds']:
