8bf8caa5ab68a6c39340306052c7e1d63f74c7ae
danerini2019 <danerini@gmail.com>
2021-08-08 10:42:46 -0700

    update

--- data/API_GET.py
+++ data/API_GET.py
@@ -8,7 +8,7 @@ import networkx as nx
 import numpy as np
 import pandas as pd
 # import peartree as pt
-# from shapely.geometry import Point
+from Shapely.geometry import Point
 import pprint
 
 # Bay area rectangle select
@@ -21,10 +21,10 @@ tl_query_feeds = 'https://transit.land/api/v2/rest/feeds?bbox=-122.503607,37.166
 # Brooklyn example rectangle selectß
 # tl_query = 'https://transit.land/api/v1/stops?bbox=-73.97339,40.649778,-73.946532,40.670353' 
 
-header = {'apikey':'NMmVInTVH1ctN7deZOcDy2Wzbh53GVav'}
+header = {'apikey':os.environ['TRANSIT_LAND_API_KEY']}
 
 resp_age = requests.get(tl_query_age, headers=header)
-resp_ops = requests.get(tl_query_ops, headers=header)
+resp_ops = requests.get(tl_query_ops, headers=header)   
 resp_routes = requests.get(tl_query_routes, headers=header)
 resp_stops = requests.get(tl_query_stops, headers=header)
 resp_feeds = requests.get(tl_query_feeds, headers=header)
@@ -34,24 +34,3 @@ rj_ops = resp_ops.json()
 rj_routes = resp_routes.json()
 rj_stops = resp_stops.json()
 rj_feeds = resp_feeds.json()
-
-pprint.pprint(rj_age['agencies'][0])
-pprint.pprint(rj_ops['operators'][0])
-pprint.pprint(rj_routes['routes'][0])
-pprint.pprint(len(rj_stops['stops'][0]))
-pprint.pprint(rj_feeds['feeds'][0])
-
-# Acquire list of operators in area
-# operators_name = []
-# for op in rj_ops['operators']:
-#     operators_name.append([op['name'], op['metro']])
-# pprint.pprint(operators_name)
-
-# Append number of stops and routes for each operator...coming soon
-
-# Create list of stops, operators, and openID numbers
-# stop_list = []
-# for stop in rj_stops['stops']:
-#     stop_list.append([stop['name'], stop['onestop_id']])
-# # pprint.pprint(stop_list)
-# print(stop_list)
