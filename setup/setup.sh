b8fa8aebd59ed531eedfed691c703311c5b84967
danerini2019 <danerini@gmail.com>
2021-08-08 14:14:08 -0700

    added pythonpath and commented out virtenv commands for now

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,20 +1,20 @@
-Not using for now!!!
+# Commented out virtual environment commands gonna use the conda environments and source this file for the variables
 
 #/bash
 # a bash script to setup the environment for the project
 
 # install pip3 and virtualenv
-sudo yum install python3-pip -y
+# sudo yum install python3-pip -y
 
 # create the virutual environment in the project root
-pip3 install virtualenv
-virtualenv -p python3 GTFS_2021
+# pip3 install virtualenv
+# virtualenv -p python3 GTFS_2021
 
 # activate the virtual environment
-source GTFS_2021/bin/activate
+# source GTFS_2021/bin/activate
 
 # install packages you will need
-pip3 install -r setup/requirements.txt
+# pip3 install -r setup/requirements.txt
 
 # Add data module location as environmental variable
 export PYTHONPATH=~/learning/realtime_GTFS/data/
