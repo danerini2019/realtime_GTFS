aee0cd5a0023d08a6df6b099b232d7ca127ceeb6
danerini2019 <danerini@gmail.com>
2021-08-18 00:02:00 -0700

    reconfigure get

--- data/API_GET.py
+++ data/API_GET.py
@@ -6,36 +6,33 @@ import time
 
 header = {'apikey':os.environ['TRANSIT_LAND_API_KEY']}
 
-# function to get a json of all stop data within rectangle
-# API endpoint not working properly. It keeps pulling data that is outside the lat/long rectangle in bbox. 
-# Problem persists for lat= long= method  
+# function to get a json of all stop data within circle or rectangle
 def get_query_stops(after):
-    # tl_query_stops = 'https://transit.land/api/v2/rest/stops?after=' + str(after) + '&bbox=-122.503607,37.166611,-121.713958,38.038060?'
-    # testing with smaller dataset
-    # tl_query_stops = 'https://transit.land/api/v2/rest/stops?after=' + str(after) + '?bbox=-122.4183,37.7758,-122.4120,37.7858'
-    tl_query_stops = 'https://transit.land/api/v2/rest/stops?after=67&?lon=-122.415304&lat=37.778309&r=10000'
-    # print(tl_query_stops)
+    tl_query_stops = 'https://transit.land/api/v2/rest/stops?after=' + str(after) + '&lon=37.816036&lat=-122.230025&radius=1000'
+    # Pull data with v1 api
+    # tl_query_stops = 'https://transit.land/api/v1/stops?per_page=10&bbox=-122.4183,37.7758,-122.4120,37.7858'
     resp_stops = requests.get(tl_query_stops, headers=header)
     rj_stops_new = resp_stops.json()
-    pprint.pprint(rj_stops_new['stops'])
     after_meta = rj_stops_new.get('meta', None)
-    if rj_stops_new['stops']:
-        pprint.pprint(rj_stops_new.keys())
+    if len(rj_stops_new['stops']) > 0:
+        print(rj_stops_new.keys())
         after = after_meta.get('after', None)
         return rj_stops_new['stops'].extend(get_query_stops(after)['stops'])
+        # TypeError: nonetype not subscriptable but everything here is not nonetype only empty
     else:
-        if 'API rate limit exceeded' in rj_stops_new['message']:
+        if after_meta and 'API rate limit exceeded' in after_meta.get('message', None):
             print(rj_stops_new['message'])
             time.sleep(45)
             rj_stops_new = get_query_stops(after)
             return rj_stops_new['stops'].extend(get_query_stops(after)['stops'])
         else:
-            print(rj_stops_new['message'])
-            return 'end'
+            pprint.pprint(rj_stops_new)
+            return rj_stops_new 
 
-stops_page_1 = get_query_stops(1)
+stops_page_1 = get_query_stops(0)
 
-pprint.pprint(stops_page_1['stops'])
+pprint.pprint(len(stops_page_1['stops']))
+pprint.pprint(stops_page_1['stops'][-1])
 
 
 
